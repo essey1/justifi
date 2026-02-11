c3ad308e82e6b1db674215f50ac45fdcc491c76f
Essey Tesfamichael Araya <esseyt3@gmail.com>
2026-02-11 07:49:42 -0500

    trying to debug

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Justifi", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
