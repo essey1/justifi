02a8445c49660bbc4a75a6eee894c904d01382ac
Essey Tesfamichael Araya <esseyt3@gmail.com>
2026-02-11 07:41:24 -0500

    put the link directly into app.tsx for debugging purposes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://justifi-qf3b.onrender.com";
 
 
 type FinancialInput = {
