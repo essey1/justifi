ac7abe241bd88356d09b0e2674ef7830b12d158f
Essey Tesfamichael Araya <esseyt3@gmail.com>
2026-02-11 07:45:27 -0500

    minor update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://justifi-qf3b.onrender.com";
+const API_URL = "https://justifi-qf3b.onrender.com/api";
 
 
 type FinancialInput = {
